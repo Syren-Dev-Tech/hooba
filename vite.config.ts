5505d904c9211da3ac0110d1a018f5ad5765ca8e
ChrisOfNormandy <chrisofnormandy@gmail.com>
2025-07-13 18:14:00 -0400

    Init actions config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/hooba/',
     plugins: [react()],
     resolve: {
         alias: {
